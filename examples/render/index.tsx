f9063de2602dde4b4ccb25dd057cb75ba18aa809
yeming <tonyyeming@didiglobal.com>
2022-04-08 14:02:19 +0800

    feat: 添加Comp组件

--- examples/render/index.tsx
+++ examples/render/index.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 
 // ReactDOM.render(<div>Hello World</div>, document.getElementById('app'));
 
-import createElement from '@lib/react-clone';
+import createElement, {Comp} from '@lib/react-clone';
 import render from '@lib/react-dom-clone';
+
+class World extends Comp {
+    render() {
+        return <span>World</span>;
+    }
+}
+const Hello = () => <span>Hello</span>
 
 const App = () => <div className="render-node">Hello World</div>;
 console.log(createElement);
-render(<App/>, document.getElementById('app'));
-render(<div className="render-node">Hello World</div>, document.getElementById('app'));
+
+render(<div><Hello/>, <World /></div>, document.getElementById('app'));
+// render(<App/>, document.getElementById('app'));
+// render(<div className="render-node">Hello World</div>, document.getElementById('app-clone'));
--- lib/react-clone.ts
+++ lib/react-clone.ts
@@ -1,3 +1,9 @@
 export default function createElement() {
     return arguments;
 }
+
+export class Comp {
+    constructor(parameters) {
+        
+    }
+}
